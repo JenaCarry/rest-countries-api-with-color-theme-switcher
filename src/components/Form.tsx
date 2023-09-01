c4a36a2144b35ac89df6a0ab8bf175a664936a28
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-09-01 19:53:44 -0300

    feat: finalizing the select

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,8 +4,21 @@ import { useState } from "react";
 import { FaAngleDown } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
 
-export function Form() {
+interface FormProps {
+  selected: string;
+  setSelected: (selected: string) => void;
+}
+
+export function Form({ selected, setSelected }: FormProps) {
   const [isActive, setIsActive] = useState(false);
+  const regions = [
+    "Africa",
+    "America",
+    "Asia",
+    "Europe",
+    "Oceania",
+    "Antarctica",
+  ];
 
   return (
     <form className="space-y-12">
@@ -22,25 +35,34 @@ export function Form() {
 
       <div className="relative font-semibold w-64 min-[481px]:w-80 space-y-4">
         <div
-          className="flex items-center justify-between bg-elements py-4 shadow-md rounded-lg px-5"
+          className="flex items-center justify-between bg-elements py-4 shadow-md rounded-lg px-5 cursor-pointer"
           onClick={() => setIsActive(!isActive)}
         >
-          <span className="text-primary-text">Filter by Region</span>
+          <span className="text-primary-text">{selected}</span>
           <FaAngleDown
             className={`text-primary-text text-base transition-all duration-200 ease-in-out ${
               isActive && "rotate-180"
             }`}
           />
         </div>
-        {isActive && (
-          <div className="w-full absolute bg-elements shadow-md rounded-lg px-5 py-5 space-y-5">
-            <div>Africa</div>
-            <div>America</div>
-            <div>Asia</div>
-            <div>Europe</div>
-            <div>Oceania</div>
-          </div>
-        )}
+        <ul
+          className={`w-full absolute bg-elements shadow-md rounded-lg z-10 overflow-hidden transition-all duration-200 ease-in-out ${
+            isActive ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-1"
+          }`}
+        >
+          {regions.map((region, index) => (
+            <li
+              key={index}
+              className="cursor-pointer px-5 py-3 hover:bg-elements-hover transition-all duration-200 ease-in-out"
+              onClick={() => {
+                setSelected(region);
+                setIsActive(false);
+              }}
+            >
+              {region}
+            </li>
+          ))}
+        </ul>
       </div>
     </form>
   );
