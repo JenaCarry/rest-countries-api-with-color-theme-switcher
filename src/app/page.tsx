6f998e71baae85425368118b8ab1e54af853aa0b
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-09-03 20:07:39 -0300

    feat: testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,28 +8,31 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [selected, setSelected] = useState<string>("Filter by Region");
+  const [show, setShow] = useState<boolean>(false);
   const [countries, setCountries] = useState<CountriesProps[]>([]);
 
   useEffect(() => {
-    getAllCountries(setCountries);
+    getAllCountries(setCountries, setShow);
   }, []);
 
   return (
     <main className="max-[375px]:px-3 px-8">
       <Form selected={selected} setSelected={setSelected} />
-      <ul className="grid grid-cols-[minmax(300px,_360px)] sm:grid-cols-[repeat(2,_minmax(300px,_320px))] lg:sm:grid-cols-[repeat(3,_minmax(300px,_320px))] min-[1440px]:grid-cols-[repeat(4,_minmax(300px,_320px))] justify-center gap-8 mt-16 pb-12">
-        {countries.map((country) => (
-          <Countries
-            key={country.name.common}
-            name={country.name.common}
-            population={country.population}
-            region={country.region}
-            capital={country.capital}
-            numericCode={country.numericCode}
-            flags={country.flags.png}
-          />
-        ))}
-      </ul>
+      {show && (
+        <ul className="grid grid-cols-[minmax(300px,_360px)] sm:grid-cols-[repeat(2,_minmax(300px,_320px))] lg:sm:grid-cols-[repeat(3,_minmax(300px,_320px))] min-[1440px]:grid-cols-[repeat(4,_minmax(300px,_320px))] justify-center gap-8 mt-16 pb-12">
+          {countries.map((country) => (
+            <Countries
+              key={country.name.common}
+              name={country.name.common}
+              population={country.population}
+              region={country.region}
+              capital={country.capital}
+              numericCode={country.numericCode}
+              flags={country.flags.png}
+            />
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
--- src/utils/handleApi.ts
+++ src/utils/handleApi.ts
@@ -1,9 +1,11 @@
 export const getAllCountries = async (
-  setCountries: React.Dispatch<React.SetStateAction<any[]>>
+  setCountries: React.Dispatch<React.SetStateAction<any[]>>,
+  setShow: (test: boolean) => void
 ) => {
   const url = "https://restcountries.com/v3.1/all";
   const res = await fetch(url);
   const data = await res.json();
 
   setCountries(data);
+  setShow(true);
 };
