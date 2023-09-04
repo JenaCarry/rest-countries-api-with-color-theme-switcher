a4098e70386284f6d86d1cda745ddd0f52f39d91
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-09-04 18:39:55 -0300

    feat: changes styles

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -9,7 +9,7 @@ export function Countries({
 }: CountryProps) {
   return (
     <>
-      <li className="bg-elements shadow-md rounded-lg overflow-hidden cursor-pointer transition-all duration-200 ease-in-out hover:-translate-y-1.5 hover:opacity-90">
+      <li className="bg-elements shadow-md rounded-lg overflow-hidden cursor-pointer transition-all duration-200 ease-in-out hover:scale-105 hover:opacity-90">
         <div className="w-full h-52">
           <img
             className="w-full h-full"
@@ -21,14 +21,16 @@ export function Countries({
         <div className="px-8 py-6 space-y-3">
           <h2 className="text-base font-extrabold pb-1.5">{name}</h2>
           <p>
-            <span className="font-semibold">Population:</span> {population}
+            <span>Population:</span> {population}
           </p>
           <p>
-            <span className="font-semibold">Region:</span> {region}
+            <span>Region:</span> {region}
           </p>
-          <p>
-            <span className="font-semibold">Capital:</span> {capital}
-          </p>
+          {capital && (
+            <p>
+              <span>Capital:</span> {capital}
+            </p>
+          )}
         </div>
       </li>
     </>
